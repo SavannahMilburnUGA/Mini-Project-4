8e3374ea727135dd2ce0b23133f00b6a33a22329
SavannahMilburnUGA <Savannah.Milburn@uga.edu>
2025-06-20 05:21:51 -0400

    Trying with 0.5 learning rate...

--- train_ASL_NNs.py
+++ train_ASL_NNs.py
@@ -18,7 +18,8 @@ start_time = time.time()
 # Create simple neural network w/ 30 hidden neurons
 simple_net = simpleNN.Network([784, 30, 24])
 # Train using SGD - same parameters as textbook digit classifier
-simple_net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+# Use lower learning rate (0.5 instead of 3.0)
+simple_net.SGD(training_data, 30, 10, 0.5, test_data=test_data)
 simple_net_time = time.time() - start_time
 simple_net_accuracy = simple_net.evaluate(test_data)  # Use evaluate() not accuracy()
 results['simple'] = {'accuracy': simple_net_accuracy, 'time': simple_net_time}
@@ -29,7 +30,8 @@ start_time = time.time()
 # Create deep neural network (3 hidden layers w/ 100, 50, 25 neurons)
 improved_net = deepNN.Network([784, 30, 24])
 # Train using SGD - same parameters as textbook digit classifier
-improved_net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+# Use lower learning rate (0.5 instead of 3.0)
+improved_net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data)
 improved_net_time = time.time() - start_time
 improved_net_accuracy = improved_net.accuracy(test_data)
 results['improved'] = {'accuracy': improved_net_accuracy, 'time': improved_net_time}
@@ -40,7 +42,8 @@ start_time = time.time()
 # Create deep neural network (3 hidden layers w/ 100, 50, 25 neurons)
 deep_net = deepNN.Network([784, 100, 50, 25, 24])
 # Train using SGD - same parameters as textbook digit classifier
-deep_net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+# Use lower learning rate (0.5 instead of 3.0)
+deep_net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data)
 deep_net_time = time.time() - start_time
 deep_net_accuracy = deep_net.accuracy(test_data)
 results['deep'] = {'accuracy': deep_net_accuracy, 'time': deep_net_time}
